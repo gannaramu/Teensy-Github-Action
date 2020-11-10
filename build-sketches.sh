5316d2d619b26316488e05a2eb4803c0191a73fe
Ram Rohit Gannavarapu <ganna.ramu@gmail.com>
2020-11-10 01:33:32 -0700

    Update build-sketches.sh

--- build-sketches.sh
+++ build-sketches.sh
@@ -5,7 +5,7 @@ examples=($(find $HOME/work -name "*.pde" -o -name "*.ino"))
 for example in "${examples[@]}"; do
   echo -n $example:
   $HOME/arduino_ide/arduino-$ARDUINO_IDE_VERSION/arduino --verify --board "teensy:avr:$DEVICE:usb=serial,speed=$SPEED,opt=o2std,keys=en-us" $example 2> error.txt > output.txt
-  local platform_switch=${PIPESTATUS[0]}
+  platform_switch=${PIPESTATUS[0]}
   # notify if the platform switch failed
   if [ $platform_switch -ne 0 ]; then
     # heavy X
