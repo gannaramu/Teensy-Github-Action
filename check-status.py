e510808bc84902ea571bf0d25977d95879810946
Ram Rohit Gannavarapu <ganna.ramu@gmail.com>
2020-11-10 02:52:21 -0700

    Update check-status.py

--- check-status.py
+++ check-status.py
@@ -1,6 +1,9 @@
-with open('error.txt') as f:
-    data = f.read()
-    if 'Opening Teensy Loader' in data:
+with open('error.txt') as f1:
+    data1 = f1.read()
+
+with open('output.txt') as f2:
+    data2 = f2.read()
+    if 'Opening Teensy Loader' in data1 or 'Opening Teensy Loader' in data2:
         print("Pass")
     else:
-        raise NameError(data)
+        raise NameError(data1 + data2)
