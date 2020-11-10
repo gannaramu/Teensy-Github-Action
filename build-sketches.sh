a0bc4c08f3b412d399f12ed818fe2c6ed8274e36
Ram Rohit Gannavarapu <ganna.ramu@gmail.com>
2020-11-10 02:08:32 -0700

    Update build-sketches.sh

--- build-sketches.sh
+++ build-sketches.sh
@@ -5,17 +5,25 @@ examples=($(find $HOME/work -name "*.pde" -o -name "*.ino"))
 for example in "${examples[@]}"; do
   echo -n $example:
   $HOME/arduino_ide/arduino-$ARDUINO_IDE_VERSION/arduino --verify --board "teensy:avr:$DEVICE:usb=serial,speed=$SPEED,opt=o2std,keys=en-us" $example 2> error.txt > output.txt
+  result=`python3 check-status.py`
+  if [ "$result" == "Pass" ]; then
+    echo -e """$GREEN""\xe2\x9c\x93"
+  else
+    echo -e """$RED""\xe2\x9c\x96"
+    echo "$result"
+    exit 1
+  fi
 #   platform_switch=${PIPESTATUS[0]}
 # notify if the platform switch failed
 #   if [ $platform_switch -ne 0 ]; then
 #     # heavy X
-#     echo -e """$RED""\xe2\x9c\x96"
+#     
 #     echo -e "\n"
 #     cat error.txt
 #     exit_code=1
 #   else
     # heavy checkmark
-    echo -e """$GREEN""\xe2\x9c\x93"
+    
     #cat output.txt
 #   fi
 done || exit 0
